882f02f02f4db8f8bef93384d6dbaad6ee638c77
Farhan-q18 <farhanqamar294@gmail.com>
2025-08-10 11:20:58 +0530

    Time: 1 ms (85.79%) | Memory: 41.6 MB (10.53%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String target = getDigitSignature(n);
+
+        for (int i = 0; i < 31; i++) {
+            int powerOf2 = 1 << i;
+            if (target.equals(getDigitSignature(powerOf2))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String getDigitSignature(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        Arrays.sort(digits);
+        return new String(digits);
+    }
+}
