49d1c5016cf0be33cbe683b8294ce8d7a5ddea0c
Farhan-q18 <farhanqamar294@gmail.com>
2026-06-04 15:13:58 +0530

    Time: 34 ms (54.55%) | Memory: 62.2 MB (39.95%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,67 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean endOfWord;
+}
+
+class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+        TrieNode curr = root;
+
+        for(int i = 0; i < word.length(); i++) {
+
+            int idx = word.charAt(i) - 'a';
+
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+
+        TrieNode curr = root;
+
+        for(int i = 0; i < word.length(); i++) {
+
+            int idx = word.charAt(i) - 'a';
+
+            if(curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        return curr.endOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        TrieNode curr = root;
+
+        for(int i = 0; i < prefix.length(); i++) {
+
+            int idx = prefix.charAt(i) - 'a';
+
+            if(curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        return true;
+    }
+}
