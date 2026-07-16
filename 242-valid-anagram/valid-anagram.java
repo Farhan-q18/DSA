f7edf5ff4cbed5d5786c6b2936bb32f28c3a6af6
Farhan-q18 <farhanqamar294@gmail.com>
2026-07-17 03:02:59 +0530

    Time: 4 ms (82.19%) | Memory: 44.5 MB (67.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,21 +3,20 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i) - 'a'] ++;
         }
-        for(int i = 0;i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch)){
-                return false;
-            }
-            map.put(ch,map.get(ch)-1);
-            if(map.get(ch) == 0){
-                map.remove(ch);
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i) - 'a'] --;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i] != 0){
+            return false;
             }
         }
-        return map.isEmpty();
+        return true;
+
+        
     }
 }
