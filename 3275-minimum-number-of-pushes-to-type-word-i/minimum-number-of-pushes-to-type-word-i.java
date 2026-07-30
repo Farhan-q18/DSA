cf13dfe5d0db6753396819c3943890c63b31f122
Farhan-q18 <farhanqamar294@gmail.com>
2026-07-30 23:21:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (47.13%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -3,7 +3,7 @@ class Solution {
         int result = 0;
         int n = word.length();
         for(int i=0;i<n;i++){
-            result += (i/8) + 1;
+            result += (i/8) + 1;    //pehle sb m traverse kro or sbko single word dedo fir agar sab place fil hojaye to + 1 krdo or return krdo for eg agar number 8 h toh hr ek number m word gaya or agar uska max limit reach hogaya toh fir + 1 add krdenge;
         }
         return result;
         
