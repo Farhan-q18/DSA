93433914287597aaa38a2e250f40573ab9277d82
Farhan-q18 <farhanqamar294@gmail.com>
2026-07-31 23:18:26 +0530

    Time: 10 ms (66.76%) | Memory: 48.1 MB (32.08%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int ans = 0;
+        int n = word.length();
+        int[] freq = new int[26];
+
+        for(char ch : word.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        Arrays.sort(freq);
+        int count = 0;
+        int minKey = 0;
+        for(int i=25;i>=0;i--){
+            int val = freq[i] * ((count/8)+1);
+            minKey += val;
+            count++;
+        }
+        return minKey;
+
+    }
+}
